6f46c4fb436253139f8732950a4dfcd7467f4f0d
Muhammed0910 <115030918+Muhammed0910@users.noreply.github.com>
2023-01-21 19:19:01 +0400

    Add files via upload

---
+++ zadaniye 25/main.js
@@ -0,0 +1,40 @@
+//1 ?
+//2
+let myAwesomeArray = '###++#+'
+let x
+let y
+function myAwesomeArray.forEach(element => {
+    if (item = '#') {
+        x + 1
+        return x
+    } else if (item = '+') {
+        y + 1
+        return y
+    }
+});
+console.log(x, y)
+//3
+var Text = 'Salam, Aleykum, Kak, Dela';
+var firstChar = Text.substr(0, 1).toUpperCase();
+alert(firstChar);
+//a yeshe luchshe
+var Text = 'Salam, Aleykum, Kak, Dela';
+alert(Text[0]);
+//4
+var str = "hAPPY bIRTHDAY";
+
+function change_register(str) {
+    var new_str = "";
+    var len = str.length;
+    for (var i = 0; i < len; i++) {
+        if (str[i] === str[i].toLowerCase()) {
+            new_str += str[i].toUpperCase();
+        } else {
+            new_str += str[i].toLowerCase();
+        }
+    }
+    return new_str;
+}
+
+document.writeln(change_register(str));
+// 5 ?
